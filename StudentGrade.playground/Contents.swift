070cb68eae13b6ecbbd9594a6ffddfdedc3ad13d
EllafCS <elafal.jaramez2018@gmail.com>
2024-08-02 00:31:15 +0300

    Initial Commit

---
+++ StudentGrade.playground/Contents.swift
@@ -0,0 +1,18 @@
+var grade : Int = 90
+
+if (grade >= 90){
+    print ("Exellent")
+}
+else if (grade >= 80){
+    print ("Very Good")
+}
+else if (grade >= 70){
+    print ("Good")
+}
+else if (grade >= 60){
+    print ("Pass")
+}
+else if (grade <= 60){
+    print ("Fail")
+}
+
